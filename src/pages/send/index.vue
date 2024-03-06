4f2cc127a91495ce42042d861c9635c6111eeeed
DESKTOP-HDOSF03\Administrator <bernovikes@hotmail.com>
2024-03-06 12:29:35 +0800

    fix:修复从赠送列表到详情不显示二维码

--- src/pages/send/index.vue
+++ src/pages/send/index.vue
@@ -8,59 +8,62 @@
 	</view>
 </template>
 <script setup>
-	import { setClipboardData } from '@/uni_modules/u-clipboard/js_sdk'
-	import { onLoad, onUnload } from '@dcloudio/uni-app'
-	import uQRCode from '@/components/uqrcode/uqrcode.vue'
-	import { fetchRedeemOrderDetail } from '@/api/api.js'
-	import { ref } from 'vue'
-	const domain = import.meta.env.VITE_DOMAIN
-	const link = ref('')
-	let timer = null
-	const copystatus = ref(false)
-	const createLink = (redeem_code,id) => {
-		link.value = `${domain}/pages/answer/index?pay_redeem_code=${redeem_code}&id=${id}`
+import { setClipboardData } from '@/uni_modules/u-clipboard/js_sdk'
+import { onLoad, onUnload } from '@dcloudio/uni-app'
+import uQRCode from '@/components/uqrcode/uqrcode.vue'
+import { fetchRedeemOrderDetail } from '@/api/api.js'
+import { ref } from 'vue'
+const domain = import.meta.env.VITE_DOMAIN
+const link = ref('')
+let timer = null
+const copystatus = ref(false)
+const createLink = (redeem_code, id) => {
+	link.value = `${domain}/pages/answer/index?pay_redeem_code=${redeem_code}&id=${id}`
+}
+onLoad((options) => {
+	const { redeem_code, trace_no, id } = options
+	if (trace_no && !redeem_code) {
+		timer = setInterval(() => {
+			init(trace_no)
+		}, 2000)
 	}
-	onLoad((options) => {
-		const { redeem_code, trace_no } = options
-		createLink(redeem_code)
-		if (trace_no && !redeem_code) {
-			timer = setInterval(() => {
-				init(trace_no)
-			}, 2000)
-		}
+	if (redeem_code && id) {
+		copystatus.value = true
+		createLink(redeem_code, id)
+	}
 
-	})
-	onUnload(() => {
-		timer && clearInterval(timer)
-		timer = null
-	})
-	const init = async (trace_no) => {
-		try {
-			const { data } = await fetchRedeemOrderDetail(trace_no)
-			if (data?.redeem_code) {
-				copystatus.value = true
-				createLink(data?.redeem_code, data?.question_bank_id)
-				clearInterval(timer)
-			}
-		} catch (e) {
-			//TODO handle the exception
+})
+onUnload(() => {
+	timer && clearInterval(timer)
+	timer = null
+})
+const init = async (trace_no) => {
+	try {
+		const { data } = await fetchRedeemOrderDetail(trace_no)
+		if (data?.redeem_code) {
+			copystatus.value = true
+			createLink(data?.redeem_code, data?.question_bank_id)
+			clearInterval(timer)
 		}
+	} catch (e) {
+		//TODO handle the exception
 	}
-	const copyAddress = () => {
-		setClipboardData(link.value).then(() => {
-			uni.showToast({
-				title: '复制成功',
-				icon: 'none'
-			})
+}
+const copyAddress = () => {
+	setClipboardData(link.value).then(() => {
+		uni.showToast({
+			title: '复制成功',
+			icon: 'none'
 		})
-	}
+	})
+}
 </script>
 <style scoped lang="scss">
-	.btn {
-		width: 250px;
-		line-height: 46px;
-		background: #007aff;
-		border-radius: 5px;
-		cursor: pointer;
-	}
+.btn {
+	width: 250px;
+	line-height: 46px;
+	background: #007aff;
+	border-radius: 5px;
+	cursor: pointer;
+}
 </style>
